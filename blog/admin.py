2257279a4ac80a87f9a23bd0b879682425793ddc
WangaiJM <jahkeyjohn@gmail.com>
2023-01-13 09:21:29 +0300

    blog admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+class BlogAdminArea(admin.AdminSite):
+    site_header = 'Blog Admin Area'
+
+blog_admin_site = BlogAdminArea(name='Blog Admin Area')
