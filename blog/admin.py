26cbefb65bdd850188d9108dc2c6acb72fc6ec33
WangaiJM <jahkeyjohn@gmail.com>
2023-01-13 09:49:50 +0300

    overriding default admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+
 class BlogAdminArea(admin.AdminSite):
     site_header = 'Blog Admin Area'
 
--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from blog.admin import blog_admin_site
 
 urlpatterns = [
-    path('blog_admin/', blog_admin_site.urls),
     path('admin/', admin.site.urls),
+    path('blog_admin/', blog_admin_site.urls),
 ]
 
-admin.site.index_title = 'The Bookstore'
-admin.site.site_header = 'Bookstore Admin'
-admin.site.site_title = 'Bookstore'
+# admin.site.index_title = 'The Bookstore'
+# admin.site.site_header = 'Bookstore Admin'
+# admin.site.site_title = 'Bookstore'
